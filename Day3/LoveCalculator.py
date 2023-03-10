439e1172f9087d405c77c061e484a3f2b46a6771
csjerry <ashutoshparihar0@gmail.com>
2023-03-10 18:55:10 +0530

    love score

--- Day3/LoveCalculator.py
+++ Day3/LoveCalculator.py
@@ -13,10 +13,10 @@ for i in range (len(combined_string)):
     if combined_string[i]== "t" or combined_string[i]== "r" or combined_string[i]== "u" or combined_string[i]== "e":
         count2+=1
 
-love_score=str(count2)+str(count1)
-if int(love_score)<10 or int(love_score)>90:
+love_score=int(str(count2)+str(count1))
+if love_score<10 or love_score>90:
     print(f"Your score is {love_score}, you go together like coke and mentos !")
-elif int(love_score)>40 and int(love_score)<50:
+elif love_score>=40 and love_score<=50:
     print(f"Your score is {love_score}, you are all good together!")
 else:
     print(f"Your score is {love_score}!")
