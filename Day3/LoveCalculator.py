e3e14d678ffc88f2297f28e25e02702562bc8640
csjerry <ashutoshparihar0@gmail.com>
2023-03-10 18:52:54 +0530

    love score

---
+++ Day3/LoveCalculator.py
@@ -0,0 +1,22 @@
+print("---------Welcome to Love Calculator!----------")
+
+person1=input("Please enter the name of first person\n").lower()
+person2=input("Please enter the name of second person\n").lower()
+
+combined_string = person1+person2
+count1=0
+count2=0
+
+for i in range (len(combined_string)):
+    if combined_string[i]== "l" or combined_string[i]== "o" or combined_string[i]== "v" or combined_string[i]== "e":
+        count1+=1
+    if combined_string[i]== "t" or combined_string[i]== "r" or combined_string[i]== "u" or combined_string[i]== "e":
+        count2+=1
+
+love_score=str(count2)+str(count1)
+if int(love_score)<10 and int(love_score)>90:
+    print(f"Your score is {love_score}, you go together like coke and mentos !")
+elif int(love_score)>40 and int(love_score)<50:
+    print(f"Your score is {love_score}, you are all good together!")
+else:
+    print(f"Your score is {love_score}!")
